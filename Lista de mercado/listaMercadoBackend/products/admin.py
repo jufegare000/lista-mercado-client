717cb29c66d8f6db3dfd4f1b44febd9e15c87da5
Alejandro <alemg-dev@hotmail.com>
2019-04-22 16:44:26 -0500

    modelos y administrador

--- Lista de mercado/listaMercadoBackend/products/admin.py
+++ Lista de mercado/listaMercadoBackend/products/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Product, SalesCheck
+
 
 # Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'value')
+    ordering = ('name', 'value')
+    search_fields = ('name', 'description')
+
+
+class SalesCheckAdmin(admin.ModelAdmin):
+    list_display = ('date', 'totalToPay')
+    ordering = ('date',)
+    search_fields = ('date', 'totalToPay')
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(SalesCheck, SalesCheckAdmin)
--- Lista de mercado/listaMercadoBackend/products/models.py
+++ Lista de mercado/listaMercadoBackend/products/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
+
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=64, verbose_name="Nombre")
+    value = models.BigIntegerField(verbose_name="Valor")
+    description = models.TextField(verbose_name="Descripción")
+    stock = models.IntegerField(verbose_name="Existencia")
+    available = models.BooleanField(verbose_name="Disponible")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Producto"
+        verbose_name_plural = "Productos"
+
+
+class Line(models.Model):
+    totalValue = models.BigIntegerField(verbose_name="Valor total")
+    product = models.ForeignKey(Product, verbose_name="Producto", related_name="get_product", on_delete=models.PROTECT)
+    cuantity = models.IntegerField(verbose_name="Cantidad")
+
+
+class SalesCheck(models.Model):
+    totalToPay = models.BigIntegerField(verbose_name="Total a pagar")
+    lines = models.ManyToManyField(Line, verbose_name="Linea")
+    date = models.DateTimeField(verbose_name="Fecha de venta", auto_now_add="true")
+
+    def __str__(self):
+        return self.date
+
+    class Meta:
+        verbose_name = "Factura"
+        verbose_name_plural = "Facturas"
